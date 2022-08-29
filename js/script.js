14090bd87a533131402cae00e04349666b776c93
braulioportela79 <braulioportela@outlook.com>
2022-08-29 18:35:42 -0300

    Handle URL error

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 let recipes = [];
 
 const addRecipe = e => {
-  e.preventDefault();
+
   let recipe = {
+    id: new Date(),
     title: document.getElementById('recipe-title').value,
     image: document.getElementById('recipe-image').value,
     cookTime: document.getElementById('cook-time').value,
@@ -11,110 +12,129 @@ const addRecipe = e => {
     description: document.querySelector('.nicEdit-main').innerHTML
   };
 
-  recipes.push(recipe);
-
-  document.querySelector('form').reset();
-
-  clearTextArea();
-
-  const footer = document.querySelector('footer');
-  const recipeContainer = document.querySelector('.recipes-container');
-  const card = document.createElement('div');
-  recipeContainer.appendChild(card);
-  card.classList.add('recipe');
-
-  if (recipes.length > 0) {
-    footer.style.position = 'relative';
-    recipeContainer.style.padding = '100px 20px';
-  }
-  // console.log(imgURL.currentSrc)
 
 
+  const form = document.getElementById('form');
 
-  // const imgURL = document.querySelector('.recipe-img img');
-  // console.log(imgURL.getAttribute('src'));
-  // console.log(imgURL)
-  // imgURL.forEach(e => {
-  // if(e.currentSrc == '') {
-  //   console.log('nao tem nada')
-  // }
-  //   console.log(e.currentSrc)
-  // })
+  const isFormValid = form.checkValidity();
 
+  if (!isFormValid) {
+    form.reportValidity();
+  } else {
 
-  recipes.forEach(e => {
+    recipes.push(recipe);
 
-    let recipeTitle = e.title
-    let recipeImage = e.image
-    let cookTime = e.cookTime;
-    let ingredients = e.ingredients;
-    let servings = e.servings;
-    let description = e.description;
+    form.checkValidity();
+    form.reset();
 
-    // console.log(recipeImage)
-    card.innerHTML = `
-        <div class="recipe-img">
+    e.preventDefault();
+
+    clearTextArea();
+
+    const footer = document.querySelector('footer');
+    const recipeContainer = document.querySelector('.recipes-container');
+    const card = document.createElement('div');
+
+    recipeContainer.appendChild(card);
+    card.classList.add('recipe');
+
+    if (recipes.length > 0) {
+      footer.style.position = 'relative';
+      recipeContainer.style.padding = '100px 20px';
+    }
+
+    recipes.forEach(e => {
+
+      let recipeIndex = e.id;
+      let recipeTitle = e.title;
+      let recipeImage = e.image;
+      let cookTime = e.cookTime;
+      let ingredients = e.ingredients;
+      let servings = e.servings;
+      let description = e.description;
+
+      card.innerHTML = `
+      <div class="recipe-img">
         <img src="${recipeImage}" alt="" />
       </div>
-  <div class="recipe-content">
-    <div class="content-header">
+      <div class="recipe-content">
+      <div class="content-header">
       <div class="row-wrapper">
-        <h2 class="recipe-title">${recipeTitle}</h2>
+      <h2 class="recipe-title">${recipeTitle}</h2>
       </div>
       <ul class="recipe-details">
-        <li class="recipe-details-item time">
-          <ion-icon name="time-outline"></ion-icon>
-          <span class="values">${cookTime}</span>
-          <span class="title">Minutos</span>
-        </li>
-        <li class="recipe-details-item ingredients">
-          <ion-icon name="book-outline"></ion-icon>
-          <span class="values">${ingredients}</span>
-          <span class="title">Ingredientes</span>
-        </li>
-        <li class="recipe-details-item servings">
-          <ion-icon name="person-outline"></ion-icon>
-          <span class="values">${servings}</span>
-          <span class="title">Porções</span>
-        </li>
+      <li class="recipe-details-item time">
+      <ion-icon name="time-outline"></ion-icon>
+      <span class="values">${cookTime}</span>
+      <span class="title">Minutos</span>
+      </li>
+      <li class="recipe-details-item ingredients">
+      <ion-icon name="book-outline"></ion-icon>
+      <span class="values">${ingredients}</span>
+      <span class="title">Ingredientes</span>
+      </li>
+      <li class="recipe-details-item servings">
+      <ion-icon name="person-outline"></ion-icon>
+      <span class="values">${servings}</span>
+      <span class="title">Porções</span>
+      </li>
       </ul>
-    </div>
-    <p class="recipe-description">${description}</p>
-    <div class="recipe-footer">
-      <a href="">Ver Receita</a>
-    </div>
-  </div>
-    `
-  })
-  const imgURL = document.querySelectorAll('.recipe-img img');
-  imgURL.forEach(e => {
+      </div>
+      <p class="recipe-description">${description}</p>
+      
+      </div>
+      <div class="recipe-footer">
+      <button id="deleteBtn">Remover Receita</button>
+      </div>
+      `
+    })
+    // const deleteBtn = document.querySelectorAll('#deleteBtn');
 
-window.location
+    // deleteBtn.forEach(btn => {
+    //   btn.addEventListener('click', () => {
 
-    var url = e.getAttribute('src');
-    var http = new XMLHttpRequest();
-console.log(url)
-http.open('HEAD', url, false)
-http.send()
-console.log(http);
-    
-    // console.log(http.open())
-    // console.log(http.onerror)
-    // console.log(http.responseText)
-    if (http.status === 404) {
-      e.setAttribute('src', './image/default-recipe-img.jpg');
-    } else if (e.getAttribute('src') == '' ) {
-      e.setAttribute('src', './image/default-recipe-img.jpg')
-    };
-    
-    
-  })
+    //     let index = recipes.indexOf(recipe.id)
 
-};
+    //     console.log(index)
+    //     // console.log(recipe.id)
+    //     // console.log(recipes.indexOf(index, 1))
 
-function clearTextArea() {
-  document.querySelector('.nicEdit-main').innerHTML = '';
 
+    //     // console.log(recipes.splice(index, 1))
+
+    //     // console.log(index)
+    //     // console.log(e)
+    //     // console.log(index)
+    //     // console.log(btn)
+    //   })
+
+    //   // render()
+    // });
+
+    // function deleteRecipe(id) {
+    //   let index = recipes.findIndex(e => e.id == id);
+    //   recipes.splice(index, 1);
+    //   render();
+    // }
+
+    const imgURL = document.querySelectorAll('.recipe-img img');
+
+    imgURL.forEach(el => {
+      var url = el.getAttribute('src');
+      const http = new XMLHttpRequest();
+      http.open('GET', url);
+      http.onerror = (e) => {
+        if (e.type === 'error') {
+          el.setAttribute('src', './image/default-recipe-img.jpg')
+        }
+      };
+      http.send();
+    });
+  };
 }
+
+let clearTextArea = () => {
+  document.querySelector('.nicEdit-main').innerHTML = '';
+};
 
 document.getElementById('send-recipe-btn').addEventListener('click', addRecipe);
